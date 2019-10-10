0f291cc402bbd513d979c50fafa446e84d63d307
azusa <411596028@qq.com>
2019-10-10 20:54:10 +0800

    增加跨域访问配置文件CorsConfig

---
+++ src/main/java/com/zsj/config/CorsConfig.java
@@ -0,0 +1,24 @@
+package com.zsj.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author zsj
+ * @date 2019.10.10
+ */
+@Configuration
+public class CorsConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*");//允许域名访问，如果*，代表所有域名
+            }
+        };
+    }
+}
